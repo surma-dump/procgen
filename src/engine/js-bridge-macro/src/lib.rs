74b42bcc9bc940241c816ad618f72a1c4195789d
Surma <surma@surma.link>
2020-05-11 23:20:51 +0100

    Make macro work

--- src/engine/js-bridge-macro/src/lib.rs
+++ src/engine/js-bridge-macro/src/lib.rs
@@ -4,10 +4,16 @@ use quote::quote;
 #[proc_macro_attribute]
 pub fn js_bridge_function(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let input = syn::parse_macro_input!(item as syn::ItemFn);
-    let name: syn::Ident = input.sig.ident;
+    let name = input.sig.ident;
+    let body = input.block;
     let result = quote! {
         #[no_mangle]
-        pub fn ohai() -> usize { 1337 }
+        pub fn #name() -> usize {
+            let v: JSVal = {
+                #body
+            };
+            v.into()
+        }
     };
     TokenStream::from(result)
 }
--- src/engine/main/src/main.rs
+++ src/engine/main/src/main.rs
@@ -4,51 +4,47 @@ use algebra::vector::Vector4;
 use js_bridge::JSVal;
 use js_bridge_macro::js_bridge_function;
 
-// struct Camera {
-//     pub position: Vector4,
-//     pub head: f32,
-//     pub pitch: f32,
-//     pub roll: f32,
-//     pub fov_y: f32,
-//     pub aspect: f32,
-//     pub far: f32,
-//     pub near: f32,
-// }
-
-// impl Camera {
-//     pub fn new() -> Camera {
-//         Camera {
-//             position: Vector4::with_values(0., 0., 0., 1.),
-//             head: 0.,
-//             pitch: 0.,
-//             roll: 0.,
-//             fov_y: 50.,
-//             aspect: 1.,
-//             far: 1e9,
-//             near: 0.01,
-//         }
-//     }
-
-//     pub fn transform(self: &Self) -> Matrix4 {
-//         Matrix4::perspective(self.fov_y, self.aspect, self.near, self.far)
-//             * Matrix4::euler(self.head, self.pitch, self.roll)
-//             * Matrix4::translation_vector(&self.position)
-//     }
-// }
-
-#[js_bridge_function]
-pub fn get_camera_matrix() -> usize {
-    // let v: JSVal = {
-    //     let c = Camera::new();
-    //     let transform = c.transform();
-    //     (&transform.fields as &[f32]).into()
-    // };
-    // v.into()
-    0
+struct Camera {
+    pub position: Vector4,
+    pub head: f32,
+    pub pitch: f32,
+    pub roll: f32,
+    pub fov_y: f32,
+    pub aspect: f32,
+    pub far: f32,
+    pub near: f32,
 }
 
-// #[no_mangle]
-// pub unsafe fn free(v: usize) {
-//     let val: JSVal = JSVal::from(v);
-//     drop(val);
-// }
+impl Camera {
+    pub fn new() -> Camera {
+        Camera {
+            position: Vector4::with_values(0., 0., 0., 1.),
+            head: 0.,
+            pitch: 0.,
+            roll: 0.,
+            fov_y: 50.,
+            aspect: 1.,
+            far: 1e9,
+            near: 0.01,
+        }
+    }
+
+    pub fn transform(self: &Self) -> Matrix4 {
+        Matrix4::perspective(self.fov_y, self.aspect, self.near, self.far)
+            * Matrix4::euler(self.head, self.pitch, self.roll)
+            * Matrix4::translation_vector(&self.position)
+    }
+}
+
+#[js_bridge_function]
+pub fn get_camera_matrix() -> JSVal {
+    let c = Camera::new();
+    let transform = c.transform();
+    (&transform.fields as &[f32]).into()
+}
+
+#[no_mangle]
+pub unsafe fn free(v: usize) {
+    let val: JSVal = JSVal::from(v);
+    drop(val);
+}
